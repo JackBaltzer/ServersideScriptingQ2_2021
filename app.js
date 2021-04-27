b33c9685bf00e2f4abe894db989cda719179012b
Jack Baltzer <jabb@tec.dk>
2021-04-27 11:13:55 +0200

    added logger and static

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const logger = require('morgan');
+app.use(logger('dev',{
+	skip: req => !req.url.endsWith('.html') && req.url.indexOf('.') > -1
+}));
+
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+app.use(express.static('./public'));
+
 app.get('/', (req, res)=>{
-	res.send('Hello World!');
+	res.render('index');
 });
 
 app.listen(port,()=>{
