998ce2f85312ce5ed3662bfbfcbfde50ed9a3598
Jack Baltzer <jabb@tec.dk>
2021-04-27 10:24:01 +0200

    nodemon

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res)=>{
-	res.send('Hello World');
+	res.send('Hello World!');
 });
 
-app.listen(3000);
+app.listen(port,()=>{
+	console.log(`Serveren kører... http://localhost:${port}`);
+});
